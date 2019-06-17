01a143fa515d6d2fb35f784c1a085bbf7d5cfffb
erikvw <ew2789@gmail.com>
2019-06-17 13:52:27 -0500

    initial

---
+++ README.rst
@@ -0,0 +1,20 @@
+|pypi| |travis| |codecov| |downloads|
+
+meta-sites
+----------
+
+
+Trial sites / recruiting centers
+
+.. |pypi| image:: https://img.shields.io/pypi/v/meta-sites.svg
+    :target: https://pypi.python.org/pypi/meta-sites
+    
+.. |travis| image:: https://travis-ci.com/meta-trial/meta-sites.svg?branch=develop
+    :target: https://travis-ci.org/meta-trial/meta-sites
+    
+.. |codecov| image:: https://codecov.io/gh/meta-trial/meta-sites/branch/develop/graph/badge.svg
+  :target: https://codecov.io/gh/meta-trial/meta-sites
+
+.. |downloads| image:: https://pepy.tech/badge/meta-sites
+   :target: https://pepy.tech/project/meta-sites
+
---
+++ setup.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+import os
+from setuptools import setup
+from setuptools import find_packages
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+with open(os.path.join(os.path.dirname(__file__), 'VERSION')) as f:
+    VERSION = f.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='meta-sites',
+    version=VERSION,
+    author=u'Erik van Widenfelt',
+    author_email='ew2789@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    url='https://github.com/meta-trial/meta-sites',
+    license='GPL license, see LICENSE',
+    description='meta sites',
+    long_description=README,
+    zip_safe=False,
+    keywords='django META sites',
+    install_requires=[
+        "edc-sites",
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
